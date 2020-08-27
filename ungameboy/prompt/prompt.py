5dd923b140872bd2353d9e4eebb567122cc7fb7e
Xavier Villaneau <xvillaneau@gmail.com>
2020-08-26 22:14:52 -0400

    Allow auto-completion of current cursor address
    Pressing TAB before typing anything when asked for an address or label
    now auto-completes with the current cursor address instead of looking
    up all labels.

--- ungameboy/prompt/prompt.py
+++ ungameboy/prompt/prompt.py
@@ -17,7 +17,6 @@ from ..commands import AddressOrLabel, LabelName, create_core_cli
 if TYPE_CHECKING:
     from prompt_toolkit.document import Document
     from .application import DisassemblyEditor
-    from ..disassembler import Disassembler
 
 
 def create_ui_cli(ugb_app: "DisassemblyEditor"):
@@ -36,42 +35,22 @@ def create_ui_cli(ugb_app: "DisassemblyEditor"):
 class LabelCompleter(Completer):
     RE_LABEL = re.compile(r'([a-zA-Z0-9_]+(\.[a-zA-Z0-9_]+)?)')
 
-    def __init__(self, asm: "Disassembler"):
-        self.asm = asm
+    def __init__(self, editor: "DisassemblyEditor"):
+        self.editor = editor
+        self.asm = editor.disassembler
 
     def get_completions(
             self, document: "Document", complete_event: CompleteEvent
     ) -> Iterable[Completion]:
         name_head = document.get_word_before_cursor(pattern=self.RE_LABEL)
+
+        if not name_head:
+            addr = self.editor.layout.main_control.cursor
+            yield Completion(str(addr))
+            return
+
         for name in self.asm.labels.search(name_head):
             yield Completion(name, -len(name_head))
-
-
-def create_cmd_completer(cmd: click.Command):
-    if cmd.params:
-        p0 = cmd.params[0].type
-        if isinstance(p0, (AddressOrLabel, LabelName)):
-            return LabelCompleter(p0.asm)
-    return None
-
-
-def create_ui_completer(cli: click.MultiCommand):
-
-    def _iter_click_groups(group):
-        opts = {}
-        for name in group.list_commands(None):
-            cmd = group.get_command(None, name)
-            if isinstance(cmd, click.MultiCommand):
-                value = _iter_click_groups(cmd)
-            elif isinstance(cmd, click.Command):
-                value = create_cmd_completer(cmd)
-            else:
-                value = None
-            opts[name] = value
-        return opts
-
-    cli_dict = _iter_click_groups(cli)
-    return NestedCompleter.from_nested_dict(cli_dict)
 
 
 class UGBPrompt:
@@ -83,7 +62,7 @@ class UGBPrompt:
             prompt="> ",
             dont_extend_height=True,
             multiline=False,
-            completer=create_ui_completer(self.cli),
+            completer=self.create_completer(),
             accept_handler=self.accept_handler,
         )
 
@@ -92,6 +71,32 @@ class UGBPrompt:
             self.prompt,
             Condition(lambda: editor.prompt_active)
         )
+
+    def create_completer(self):
+        label_complete = LabelCompleter(self.editor)
+
+        def _create_cmd_completer(cmd: click.Command):
+            if cmd.params:
+                p0 = cmd.params[0].type
+                if isinstance(p0, (AddressOrLabel, LabelName)):
+                    return label_complete
+            return None
+
+        def _iter_click_groups(group):
+            opts = {}
+            for name in group.list_commands(None):
+                cmd = group.get_command(None, name)
+                if isinstance(cmd, click.MultiCommand):
+                    value = _iter_click_groups(cmd)
+                elif isinstance(cmd, click.Command):
+                    value = _create_cmd_completer(cmd)
+                else:
+                    value = None
+                opts[name] = value
+            return opts
+
+        cli_dict = _iter_click_groups(self.cli)
+        return NestedCompleter.from_nested_dict(cli_dict)
 
     def accept_handler(self, buffer: Buffer):
         if buffer.text:
