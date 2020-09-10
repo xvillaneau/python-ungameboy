4aeffa2bee4c3c2e90e7684c3e79cf5e0836679f
Xavier Villaneau <xvillaneau@gmail.com>
2020-09-10 18:31:41 -0400

    Refactor sidebar logic for extensibility

--- ungameboy/prompt/layout.py
+++ ungameboy/prompt/layout.py
@@ -1,3 +1,5 @@
+from functools import reduce
+from operator import or_
 from typing import TYPE_CHECKING
 
 from prompt_toolkit.layout import D, HSplit, Layout, VSplit, Window
@@ -9,18 +11,28 @@ if TYPE_CHECKING:
     from .application import DisassemblyEditor
 
 
-def build_xrefs_sidebar(app: 'DisassemblyEditor'):
+def build_sidebar(app: 'DisassemblyEditor'):
 
-    bar_stack = HSplit([
+    xrefs_view = HSplit([
         Window(app.xrefs.head_control, height=1),
         Window(height=1, char='\u2500'),
         Window(app.xrefs.refs_control),
     ])
 
-    return ConditionalContainer(
-        content=VSplit([Window(width=1, char='\u2502'), bar_stack]),
-        filter=app.filters.xrefs_visible,
+    views = [
+        (xrefs_view, app.filters.xrefs_visible),
+    ]
+
+    separator = ConditionalContainer(
+        content=Window(width=1, char='\u2502'),
+        filter=reduce(or_, (filter for _, filter in views))
     )
+    stack = HSplit([
+        ConditionalContainer(content=view, filter=filter)
+        for view, filter in views
+    ])
+
+    return VSplit([separator, stack])
 
 
 class UGBLayout:
@@ -34,11 +46,11 @@ class UGBLayout:
             get_vertical_scroll=self.main_control.get_vertical_scroll,
             width=D(weight=5),
         )
-        xref_sidebar = build_xrefs_sidebar(self.editor)
+        sidebar = build_sidebar(self.editor)
 
         # noinspection PyTypeChecker
         body = HSplit([
-            VSplit([main_window, xref_sidebar]),
+            VSplit([main_window, sidebar]),
             editor.prompt.container,
         ])
 
