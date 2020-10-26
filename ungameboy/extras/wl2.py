0fea33ea578987d1a27b94990e96e6703959d594
Xavier Villaneau <xvillaneau@gmail.com>
2020-10-26 00:06:27 -0400

    Add Wario Land 2 music voice data type

--- ungameboy/extras/wl2.py
+++ ungameboy/extras/wl2.py
@@ -1,8 +1,9 @@
-from typing import TYPE_CHECKING
+from bisect import bisect_right
+from typing import TYPE_CHECKING, List
 
 from ..address import Address
 from ..data_types import Byte, SignedByte
-from ..dis.data import Data, DataProcessor, DataTable, JumpTableDetector
+from ..dis.data import Data, DataProcessor, DataTable, JumpTableDetector, RowItem
 from ..scripts import asm_script
 
 if TYPE_CHECKING:
@@ -130,3 +131,110 @@ def detect_sprites_table(asm: 'Disassembler', address: Address):
 
         asm.data.insert(WL2Sprite(ref))
         asm.labels.auto_create(ref, local=True)
+
+
+class WL2SoundVoice(Data):
+    name = "wl2.voice"
+
+    RESET_CODES = frozenset([0xb1, *range(0xb6, 0xbc), 0xc7, 0xc8, 0xcb, 0xcc])
+    DATA_CODES = frozenset([0xb5, *range(0xbc, 0xc7), 0xc9, 0xca, 0xcf])
+    ADDR_CODES = frozenset([0xb2, 0xb3])
+
+    def __init__(self, address: Address, size: int = 0, processor=None):
+        super().__init__(address, size, processor)
+        self.map: List[int] = []
+
+    def populate(self, rom: 'ROMBytes'):
+        start = offset = self.address.rom_file_offset
+        stored_code = 0
+
+        def process_code(fallback=False):
+            nonlocal offset, stored_code
+
+            if fallback:
+                code = stored_code
+            else:
+                code = rom[offset]
+                offset += 1
+            if code >= 0xbe:
+                stored_code = code
+
+            if code in self.RESET_CODES:
+                return False
+            if not fallback and code < 0x80:
+                return process_code(fallback=True)
+
+            if code == 0xcd:  # Jumptable 1
+                arg = rom[offset]
+                offset += 1
+                return arg in (*range(1, 8), 0xa, 0xb)
+
+            if code <= 0xb0 or code == 0xb4:
+                pass
+            elif code in self.DATA_CODES:
+                offset += 1
+            elif code in self.ADDR_CODES:
+                offset += 2
+            else:
+                # Remaining: code >= 0xd0 and code == 0xce
+                arg = rom[offset]
+                while arg < 0x80:
+                    offset += 1
+                    arg = rom[offset]
+
+            return True
+
+        self.map.clear()
+        self.map.append(0)
+        while process_code():
+            self.map.append(offset - start)
+
+        self.size = offset - start
+        super().populate(rom)
+
+    @property
+    def rows(self) -> int:
+        return len(self.map)
+
+    def row_at(self, address: Address) -> int:
+        if address.zone != self.address.zone:
+            raise IndexError("Address not in same zone")
+        offset = address.offset - self.address.offset
+        if not 0 <= offset < self.size:
+            raise IndexError("Address not in data")
+        return bisect_right(self.map, offset) - 1
+
+    def row_address(self, row: int) -> Address:
+        return self.address + self.map[row]
+
+    def get_row_bin(self, row: int) -> bytes:
+        start = self.map[row]
+        end = self.size if row >= self.rows - 1 else self.map[row + 1]
+        return self.data[start:end]
+
+    def get_row_items(self, row: int) -> List[RowItem]:
+        row_bin = self.get_row_bin(row)
+        code = row_bin[0]
+
+        if code == 0xb4:
+            return ["pop"]
+
+        if code in self.ADDR_CODES:
+            addr = int.from_bytes(row_bin[1:3], "little")
+            addr = Address.from_memory_address(addr)
+            name = "jump" if code == 0xb2 else "push+jump"
+            return [name, addr]
+
+        if code == 0x80:
+            return ["noop"]
+
+        if 0x81 <= code <= 0xb0:
+            index = code - 0x81
+            if index < 0x18:
+                cycles = index + 1
+            else:
+                a, b = divmod(index - 0x18, 4)
+                cycles = 0x1c + 0xc * a + 2 * (b + (b == 3))
+            return [f"wait {cycles} cycle{'s' * (cycles != 1)}"]
+
+        return [Byte(b) for b in row_bin]
